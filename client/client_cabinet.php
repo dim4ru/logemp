d1bceeb36011d8bbb118c546765c8deed158f2a0
dim4 <@>
2023-12-14 01:05:27 +0600

    refactor

--- client/client_cabinet.php
+++ client/client_cabinet.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-if (isset($_SESSION['tel'])) {
+if (!isset($_SESSION['tel'])) {
+    // Сессия не содержит информацию о пользователе
+    header("Location: client_login.php");
+} else {
     $tel = $_SESSION['tel'];
     echo "<h2>Личный кабинет клиента по номеру $tel</h2>";
-} else {
-    // Сессия не содержит информацию о пользователе
-    // Можно выполнить другие действия, например, перенаправить пользователя на страницу входа
-    header("Location: client_login.php");
+
+
 }
